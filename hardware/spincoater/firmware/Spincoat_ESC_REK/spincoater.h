1fa65cd091c74c29f4f13662a83dd27ed4b2b4fa
rishi-e-kumar <47284667+rishi-e-kumar@users.noreply.github.com>
2020-09-03 08:56:53 -0700

    moving average implementation, remove lcd + led code, header file

---
+++ hardware/spincoater/firmware/Spincoat_ESC_REK/spincoater.h
@@ -0,0 +1,23 @@
+/* Pin Definitions */
+#define ESC_PIN 9     // Pin 9 to LED or actual ESC
+
+/* RPM Limits */
+//values to calibrate ESC range
+#define MIN_SIGNAL 700
+#define MAX_SIGNAL 2000
+
+
+//values that will actually cause rotor to spin
+#define MIN_USABLE_SIGNAL 802
+#define JUMPSTART_SIGNAL 840 // value below which the rotor will stall unless already spinning
+#define MAX_USABLE_SIGNAL 1900
+
+#define MIN_RPM 1000
+#define JUMPSTART_RPM_CUTOFF 1400
+#define MAX_RPM 7000
+
+#define JUMPSTART_DELAY 1000 //ms, time to wait between jumpstarting and going to target rpm
+
+/* RPM Measuring */
+#define RPM_WINDOW_SIZE 5 //number of rpm readings to average
+#define MOTOR_POLES 14 // number of magnetic poles present in BLDC motor
