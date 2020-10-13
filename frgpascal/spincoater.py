8c7e49ea2ea0d609c39492ae029aecea1c4e0aed
rishi-e-kumar <47284667+rishi-e-kumar@users.noreply.github.com>
2020-10-13 11:55:19 -0700

    update sc

--- frgpascal/spincoater.py
+++ frgpascal/spincoater.py
@@ -2,33 +2,34 @@ import serial
 import time
 
 class SpinCoater:
-	def __init__(self, port = 'INSERTDEFAULTPORTHERE'):
+	def __init__(self, port = '/dev/ttyACM5'):
 		#constants
-		self.POLLINGRATE = 0.5 #query rate to arduino, in seconds
-		self.ACCELERATIONRANGE = (1,200) #rpm/s
+		self.POLLINGRATE = .5 #query rate to arduino, in seconds
+		# self.ACCELERATIONRANGE = (1,200) #rpm/s
 		self.SPEEDRANGE = (1000, 9000) #rpm
 		self.TERMINATOR = '\n'
-
-		self.connect(port = port)
+		self.port = port
+		self.BAUDRATE = 57600
+		self.connect() 
 		self.unlock()
+
+	def connect(self, **kwargs):
+		self.__handle = serial.Serial(port = self.port, baudrate=self.BAUDRATE,**kwargs)
+
+	# def connect(self, port = None, **kwargs):
+	# 	if port is None:
+	# 	    port = self.port
+	# 	self.__handle = serial.Serial(port = port, baudrate=self.BAUDRATE,**kwargs)
 
 	@property
 	def rpm(self):
-		self.write('c') #command to read rpm
+		self.write(f'c') #command to read rpm
 		self.__rpm = float(self.__handle.readline().strip())
 		return self.__rpm
 	
-	def connect(self, port, **kwargs):
-		self.__handle = serial.Serial(
-			port = port,
-			**kwargs
-			)
-		self.__handle.open()
-		# routine to initialize spincoater 
-		# - cycle ESC power
-		# - pwm to low, 2 seconds
-		# - pwm to high, x seconds
-		# finish
+	# def connect(self, port, baudrate, **kwargs):
+	# 	self.__handle = serial.Serial(port = self.port, baudrate=self.baudrate,**kwargs)
+
 
 	def disconnect(self):
 		self.__handle.close()
@@ -38,7 +39,7 @@ class SpinCoater:
 		appends terminator and converts to bytes before sending message to arduino
 		'''
 		self.__handle.write(f'{s}{self.TERMINATOR}'.encode())
- 	
+	
 	def lock(self):
 		"""
 		routine to lock rotor in registered position for sample transfer
@@ -46,10 +47,35 @@ class SpinCoater:
 		if self.locked:
 			return
 
-		self.setspeed(min(self.SPEEDRANGE))
-		time.sleep(1)
-		self.setspeed(0, 1) #slowly decelerate to stop
-		self.write('l') #send command to engage electromagnet
+		self.write('z') # 
+		time.sleep(2) #wait some time to ensure rotor has stopped and engaged with electromagnet
+		self.write('i4') #send command to engage electromagnet
+		time.sleep(2) #wait some time to ensure rotor has stopped and engaged with electromagnet
+		self.locked = True
+
+	def motor_on(self):
+		"""
+		routine to lock rotor in registered position for sample transfer
+		"""
+		if self.locked:
+			return
+
+		self.write('z') # 
+		time.sleep(2) #wait some time to ensure rotor has stopped and engaged with electromagnet
+		self.write('i3') #send command to engage electromagnet
+		time.sleep(2) #wait some time to ensure rotor has stopped and engaged with electromagnet
+		self.locked = True
+
+	def motor_off(self):
+		"""
+		routine to lock rotor in registered position for sample transfer
+		"""
+		if self.locked:
+			return
+
+		self.write('z') # 
+		time.sleep(2) #wait some time to ensure rotor has stopped and engaged with electromagnet
+		self.write('o3') #send command to engage electromagnet
 		time.sleep(2) #wait some time to ensure rotor has stopped and engaged with electromagnet
 		self.locked = True
 
@@ -57,31 +83,33 @@ class SpinCoater:
 		"""
 		unlocks the rotor from registered position
 		"""
-		self.write('u') #send command to disengage electromagnet
+		self.write('o4') #send command to disengage electromagnet
+		time.sleep(2) #wait some time to ensure rotor has unlocked before attempting to rotate 
 		self.locked = False
 
-	def setspeed(self, speed, acceleration = max(self.ACCELERATIONRANGE)):
-		"""
+	def setspeed(self, speed): #acceleration = max(self.ACCELERATIONRANGE)):
+		
+		speed = int(speed) #arduino only takes integer inputs
+
+		if self.locked:
+			self.unlock()
+		self.__handle.write(f'a{speed:d}'.encode()) 
+
+		#send command to arduino. assumes arduino responds to "s{rpm},{acceleration}\r'
+		'''
 		sends commands to arduino to set a target speed with a target acceleration
 
 		speed - target angular velocity, in rpm
 		acceleration - target angular acceleration, in rpm/second. always positive
-		"""
-		speed = int(speed) #arduino only takes integer inputs
 		acceleration = int(acceleration) 
-
-		if self.locked:
-			self.unlock()
-
-		self.__handle.write(f's{speed:d},{acceleration:d}') #send command to arduino. assumes arduino responds to "s{rpm},{acceleration}\r'
-
 		#possible code to wait for confirmation response from arduino that speed was hit successfully
+		'''
 
 	def stop(self):
 		"""
 		stop rotation and locks the rotor in position
 		"""
-		self.setspeed(min(self.SPEEDRANGE), 1)
+		self.write('z') # 
 		self.lock()
 
 	def recipe(self, recipe):
@@ -106,24 +134,29 @@ class SpinCoater:
 			'rpm': []
 			}
 
-		start_time = time.time()
+		start_time = round(time.time()) #big ass number
 		next_step_time = 0
 		time_elapsed = 0
+		# first step == true
 		for step in recipe:
 			speed = step[0]
-			acceleration = step[1]
-			duration = step[2]
+			duration = step[1]
+
+			# if first_step == true:
+
+
+			# 	first_step == false
+			# self.write('d') 
+			self.setspeed(speed)
+			next_step_time += duration
 
 			while time_elapsed <= next_step_time:
+				time_elapsed = time.time()-start_time
 				record['time'].append(time_elapsed)
 				record['rpm'].append(self.rpm)
 				time.sleep(self.POLLINGRATE)
 
-			self.setspeed(speed, acceleration)
-			next_step_time += duration
-		self.stop()
+			# self.write('f')
+		self.lock()
 
 		return record
-
-
-
--- hardware/spincoater/firmware/Spincoat_ESC_REK_v2/spincoater.h
+++ hardware/spincoater/firmware/Spincoat_ESC_REK_v2/spincoater.h
@@ -3,21 +3,20 @@
 
 /* RPM Limits */
 //values to calibrate ESC range
-#define MIN_SIGNAL 700
-#define MAX_SIGNAL 2000
+#define MIN_SIGNAL 700 //700
+#define MAX_SIGNAL 2000 //2000
 
 
 //values that will actually cause rotor to spin
-#define MIN_USABLE_SIGNAL 802
-#define JUMPSTART_SIGNAL 840 // value below which the rotor will stall unless already spinning
-#define MAX_USABLE_SIGNAL 1900
+#define MIN_USABLE_SIGNAL 802 //802
+#define JUMPSTART_SIGNAL 840 //840 value below which the rotor will stall unless already spinning
+#define MAX_USABLE_SIGNAL 1900 //1900
 
-#define MIN_RPM 1000
+#define MIN_RPM 1000 //1000
 #define JUMPSTART_RPM_CUTOFF 1400
 #define MAX_RPM 7000
-
 #define JUMPSTART_DELAY 1000 //ms, time to wait between jumpstarting and going to target rpm
 
 /* RPM Measuring */
-#define RPM_WINDOW_SIZE 5 //number of rpm readings to average
+#define RPM_WINDOW_SIZE 3 //number of rpm readings to average
 #define MOTOR_POLES 14 // number of magnetic poles present in BLDC motor
