3d742f047ee612fe062e7287f0aed709406d71a5
Rishi E Kumar <47284667+rekumar@users.noreply.github.com>
2021-08-22 12:46:44 -0700

    worklist builder first draft

--- frgpascal/experimentaldesign/tasks.py
+++ frgpascal/experimentaldesign/tasks.py
@@ -17,6 +17,7 @@ from frgpascal.workers import (
     Worker_Storage,
 )
 
+
 gg = Worker_GantryGripper(planning=True)
 sclh = Worker_SpincoaterLiquidHandler(planning=True)
 hp = Worker_Hotplate(n_workers=25, planning=True)
