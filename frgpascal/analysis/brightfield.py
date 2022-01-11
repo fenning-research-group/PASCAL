8192e1c7da78c6f5875459b25373fab9b3f90c67
jack-palm <87740914+jack-palm@users.noreply.github.com>
2022-01-11 13:45:12 -0800

    add updated brightfield ad darkfield scripts

--- frgpascal/analysis/brightfield.py
+++ frgpascal/analysis/brightfield.py
@@ -1,29 +1,56 @@
-import tifffile
+from tifffile import imread as ir
 import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.image import resize as rz
+import dill
+import os
 
+MODULE_DIR = os.path.dirname(__file__)
 
 def load(fid):
-    img = tifffile.imread(fid)
-    return img * 64  # rescales to 0-1
+    """
+    Loads an image file from a given fid,
+    then converts it from float64 to float32
+    """
+    img = ir(fid)*64*255
+    return img.astype(np.float32)
 
 
 class SampleChecker:
     def __init__(self):
+        self.THRESHOLD = 0.5
         self._load_model()
 
     def _load_model(self):
         """
-        loads pretrained Keras classifier to identify dropped samples
+        loads pretrained Keras binary classifier and ImageDataGenerator to identify dropped samples
         by brightfield imaging
         """
-        return
-
+        self.model = load_model(os.path.join(MODULE_DIR, "datafiles", "MissingPresent_Model.h5"))
+        
+        with open(
+            os.path.join(MODULE_DIR, "datafiles", "datagen.pkl"), "rb"
+        ) as f:
+            self.datagen = dill.load(f)  
+        )
+        
     def sample_is_present(self, img) -> bool:
         """
         given a PASCAL brightfield image, identifies whether or not
         a sample was present in the characterization stage.
 
-        True = sample is detected on stage
-        False = sample was not detected on stage. probably dropped earlier in the protocol
+        True = sample is detected on stage, >0.5 probability
+        False = sample was not detected on stage. probably dropped earlier in the protocol, <0.5 probability
         """
-        return True
+        # use the ImageDataGenerator to preprocess the image
+        img  = self.datagen.standardize(img)
+        # resize image to match model input
+        img = rz(img, [200, 200])
+        img = np.expand_dims(img, axis=0)
+        # predict the probability of sample being present
+        ans = self.model.predict(img)
+        # return bool and the probability
+        if ans[0][0] > self.THRESHOLD:
+            return True, ans[0][0]
+        else:
+            return False, ans[0][0]
--- frgpascal/analysis/darkfield.py
+++ frgpascal/analysis/darkfield.py
@@ -1,12 +1,89 @@
-import tifffile
+from tifffile import imread as ir
 import numpy as np
+from skimage.filters import threshold_local
+import frgtools.misc as frgm
+from natsort import natsorted
 
+""""
+A collection of functions for analyzing darkfield images.
+"""
 
-def load(fid):
-    img = tifffile.imread(fid)
-    return img * 64  # rescales to 0-1
+def load_fids(dir):
+    return(frgm.listdir(dir))
 
+def load_image(fid, load_red = True):
+    """
+    Load an image from a filepath. Defaults to load only the red channel.
+    """
+    img = ir(fid)*64*255
+    img = img.astype(np.float32)
+    if load_red == True:
+        return img[:,:,0]
+    else:
+        return img
 
-def bright_fraction(img):
-    THRESHOLD = 0.2
-    return np.sum(img > THRESHOLD) / np.product(img.shape)
+def get_median(im):
+    """
+    From an imported image, get the median pixel value
+    """
+    if len(im.shape) == 2:
+        pass
+    elif len(im.shape) == 3:
+        # If RGB, take red column, the first column.
+        im = im[:,:,0]
+        print('Careful! Image entered as RGB, not single channel. Red channel was used.')
+    else:
+        print('The image was not in the correct format. Must be RGB or a single channel')
+        pass
+
+    median = np.median(im)
+
+    return median
+
+def mask_intensity(im):
+    """
+    This function uses the 'threshold_local' function of scikit-image
+    to mask a given image and return the value of the pixels within the mask.
+    """
+    # set an offset to be called within the thresholding function.
+    # this was determined by trial-and-error and can certainly be changed
+    offset = -np.median(im)/3
+    # store the thresholded image to thresh
+    # the block_size is the number of pixels considered for a local threshold.
+    thresh = threshold_local(im, block_size = 541, method = 'gaussian', offset = offset)
+    # convert to binary. this is the mask.
+    b_im = im > thresh
+    # get the values of the image where b_im == 1
+    vals = im[b_im]
+    #  return the sum of the pixel values for use as a metric of film quality
+    return vals.sum()
+    
+def quality_check(im):
+    median = get_median(im)
+    thresh_val = mask_intensity(im)
+    # in a darkfield image, a lower median pixel intensity is better.
+    # the values below are determined by trial-and-error.
+    if median < 12.0:
+        med = 'excellent'
+    elif median >= 12.0 and median < 20.0:
+        med = 'good'
+    elif median >= 20.0 and median < 32.0:
+        med = 'fair'
+    elif median >=32.0:
+        med = 'poor'
+    else:
+        pass
+    # a lower summed threshold value is better for darkfield images.
+    # the values below are determined by trial-and-error.
+    if thresh_val < 100000.0:
+        thresh = 'excellent'
+    elif thresh_val >= 100000.0 and thresh_val < 200000.0:
+        thresh = 'good'
+    elif thresh_val >= 200000.0 and thresh_val < 400000.0:
+        thresh = 'fair'
+    elif thresh_val >= 400000.0:
+        thresh = 'poor'
+    else:
+        pass
+    
+    return(med, thresh)
