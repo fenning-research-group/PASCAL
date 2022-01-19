57b463716f6e672d85a0d57c3dc223497c7ab304
Rishi E Kumar <47284667+rekumar@users.noreply.github.com>
2022-01-19 14:50:29 -0800

    lets try it out!

--- frgpascal/GP.py
+++ frgpascal/GP.py
@@ -29,60 +29,63 @@ from random import shuffle
 
 from frgpascal import system
 import roboflo
+from frgpascal.bridge import ALClient
 
 
-def class_to_worker(worker_class):
-    for w in ALL_WORKERS.values():
-        if isinstance(w, worker_class):
-            return w
-    raise Exception(
-        "Could not find a matching worker for class {}".format(worker_class)
-    )
+# class BOSimulation:
 
 
-def generate_protocol(sample: Sample, system: roboflo.System, min_start: int = 0):
-    tray = [
-        w
-        for w in system.workers
-        if isinstance(w, Worker_Storage) and w.name == sample.storage_slot["tray"]
-    ][0]
-    # print(hotplate)
+# def class_to_worker(worker_class):
+#     for w in ALL_WORKERS.values():
+#         if isinstance(w, worker_class):
+#             return w
+#     raise Exception(
+#         "Could not find a matching worker for class {}".format(worker_class)
+#     )
+# def generate_protocol(sample: Sample, system: roboflo.System, min_start: int = 0):
+#     tray = [
+#         w
+#         for w in system.workers
+#         if isinstance(w, Worker_Storage) and w.name == sample.storage_slot["tray"]
+#     ][0]
+#     # print(hotplate)
 
-    for task in sample.worklist:
-        corrected_workers = []
-        for worker in task.workers:
-            if worker in system.ALL_WORKERS.values():
-                corrected_workers.append(worker)
-            elif worker == Worker_Hotplate:
-                corrected_workers.append(
-                    [
-                        w
-                        for w in system.workers
-                        if isinstance(w, Worker_Hotplate) and w.name == task.hotplate
-                    ][0]
-                )
-            elif worker == Worker_Storage:
-                corrected_workers.append(tray)
-            else:
-                corrected_workers.append(class_to_worker(worker))
-        task.workers = corrected_workers
-        task.sample = sample
+#     for task in sample.worklist:
+#         corrected_workers = []
+#         for worker in task.workers:
+#             if worker in system.ALL_WORKERS.values():
+#                 corrected_workers.append(worker)
+#             elif worker == Worker_Hotplate:
+#                 corrected_workers.append(
+#                     [
+#                         w
+#                         for w in system.workers
+#                         if isinstance(w, Worker_Hotplate) and w.name == task.hotplate
+#                     ][0]
+#                 )
+#             elif worker == Worker_Storage:
+#                 corrected_workers.append(tray)
+#             else:
+#                 corrected_workers.append(class_to_worker(worker))
+#         task.workers = corrected_workers
+#         task.sample = sample
 
-    sample.protocol = system.generate_protocol(
-        name=sample.name,
-        worklist=sample.worklist,
-        starting_worker=tray,
-        ending_worker=tray,
-        min_start=min_start,
-    )
+#     sample.protocol = system.generate_protocol(
+#         name=sample.name,
+#         worklist=sample.worklist,
+#         starting_worker=tray,
+#         ending_worker=tray,
+#         min_start=min_start,
+#     )
 
 
-class SingleTaskGP(ALBridge):
+class SingleTaskGP(ALClient):
     def __init__(self):
         #### Solution Storage Slots
         super().__init__()
-        self.system = system.build()  # TODO wtf
-        self.solution_time = 30
+        self.system = system.build()  # TODO wtf is this function name
+        self.SCHEDULE_SOLVE_TIME = 20
+        self.BUFFER_TIME = 15 #seconds of grace period to give between solution discovery and actual execution time
     def initialize_experiment(self):
         ### Hardware
         # Solution Storage
@@ -119,30 +122,36 @@ class SingleTaskGP(ALBridge):
             solvent="MethylAcetate", labware="4mL_Tray1", well="D4"
         )
 
-    def build_protocol(self, drop_volume, drop_time, drop_rate, anneal_duration, min_start = 0):
-        spincoat_absorber = Spincoat(
-            steps=[
-                [3000, 2000, 50],  # speed (rpm), acceleration (rpm/s), duration (s)
-            ],
-            drops=[
-                Drop(
-                    solution=self.absorber,  # this will be filled later using the list of psk solutions
-                    volume=20,
-                    time=-1,
-                    blow_out=True,
-                ),
-                Drop(
-                    solution=self.antisolvent,
-                    volume=drop_volume,
-                    time=drop_time,
-                    reuse_tip=True,
-                    touch_tip=False,
-                    rate=drop_rate,
-                    pre_mix=(5, 50),
-                    slow_travel=True,
-                ),
-            ],
-        )
+        self.sample_counter = 0
+
+    def build_protocol(self, anneal_duration, min_start = None):
+        min_allowable_start = self.experiment_time + self.SCHEDULE_SOLVE_TIME + self.BUFFER_TIME #cant schedule too early or we cant actually execute
+        if min_start is None:
+            min_start = min_allowable_start
+        min_start = max([min_start, min_allowable_start]) 
+        # spincoat_absorber = Spincoat(
+        #     steps=[
+        #         [3000, 2000, 50],  # speed (rpm), acceleration (rpm/s), duration (s)
+        #     ],
+        #     drops=[
+        #         Drop(
+        #             solution=self.absorber,  # this will be filled later using the list of psk solutions
+        #             volume=20,
+        #             time=-1,
+        #             blow_out=True,
+        #         ),
+        #         Drop(
+        #             solution=self.antisolvent,
+        #             volume=drop_volume,
+        #             time=drop_time,
+        #             reuse_tip=True,
+        #             touch_tip=False,
+        #             rate=drop_rate,
+        #             pre_mix=(5, 50),
+        #             slow_travel=True,
+        #         ),
+        #     ],
+        # )
         anneal_absorber = Anneal(temperature=100, duration=anneal_duration)
         
         name = f'sample{self.sample_counter}'
@@ -150,10 +159,10 @@ class SingleTaskGP(ALBridge):
             name=name,
             substrate='placeholder',
             worklist = [
-                spincoat_absorber,
+                # spincoat_absorber,
                 anneal_absorber,
-                Rest(180),
-                Characterize()
+                # Rest(180),
+                # Characterize()
             ],
             storage_slot = {'tray': self.sample_trays[0].name, 'slot': self.sample_trays[0].load(name)}
         )
@@ -162,9 +171,9 @@ class SingleTaskGP(ALBridge):
             worklist=sample.worklist,
             starting_worker = self.sample_trays[0],
             starting_worker = self.sample_trays[0],
-            min_start = min_start #TODO read this on the fly
+            min_start = min_start
         )
-        self.system.scheduler.solve(self.solution_time)
-        new_tasks = [task.to_dict() for task in sample.protocol.worklist]
-        self.websocket.add_protocol(protocol = new_tasks)
-        
+        self.system.scheduler.solve(self.SCHEDULE_SOLVE_TIME)
+        self.websocket.add_sample(sample=sample.to_dict())
+        self.sample_counter += 1
+
