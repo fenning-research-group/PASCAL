cd60e8422ed3ae592c473533b2efb0ee470d4bc3
Rishi E Kumar <47284667+rekumar@users.noreply.github.com>
2022-01-24 16:49:36 -0800

    brightfield checker + update readme for file transfer

--- frgpascal/analysis/brightfield.py
+++ frgpascal/analysis/brightfield.py
@@ -1,18 +1,19 @@
-from tifffile import imread as ir
+from tifffile import imread
 import numpy as np
 from tensorflow.keras.models import load_model
-from tensorflow.image import resize as rz
+from tensorflow.image import resize
 import dill
 import os
 
 MODULE_DIR = os.path.dirname(__file__)
+
 
 def load(fid):
     """
     Loads an image file from a given fid,
     then converts it from float64 to float32
     """
-    img = ir(fid)*64*255
+    img = imread(fid) * 64 * 255
     return img.astype(np.float32)
 
 
@@ -26,31 +27,35 @@ class SampleChecker:
         loads pretrained Keras binary classifier and ImageDataGenerator to identify dropped samples
         by brightfield imaging
         """
-        self.model = load_model(os.path.join(MODULE_DIR, "datafiles", "MissingPresent_Model.h5"))
-        
-        with open(
-            os.path.join(MODULE_DIR, "datafiles", "datagen.pkl"), "rb"
-        ) as f:
-            self.datagen = dill.load(f)  
+        self.model = load_model(
+            os.path.join(MODULE_DIR, "assets", "Brightfield_SampleChecker_Model.h5")
         )
-        
-    def sample_is_present(self, img) -> bool:
+
+        with open(
+            os.path.join(MODULE_DIR, "assets", "Brightfield_SampleChecker_datagen.pkl"),
+            "rb",
+        ) as f:
+            self.datagen = dill.load(f)
+
+    def sample_is_present(self, img, return_probability=False) -> bool:
         """
         given a PASCAL brightfield image, identifies whether or not
         a sample was present in the characterization stage.
 
-        True = sample is detected on stage, >0.5 probability
+        True = sample is detected on stage, >=0.5 probability
         False = sample was not detected on stage. probably dropped earlier in the protocol, <0.5 probability
         """
         # use the ImageDataGenerator to preprocess the image
-        img  = self.datagen.standardize(img)
+        img = self.datagen.standardize(img)
         # resize image to match model input
-        img = rz(img, [200, 200])
+        img = resize(img, [200, 200])
         img = np.expand_dims(img, axis=0)
         # predict the probability of sample being present
         ans = self.model.predict(img)
         # return bool and the probability
-        if ans[0][0] > self.THRESHOLD:
-            return True, ans[0][0]
+        if return_probability:
+            response = (ans[0][0] > self.THRESHOLD, ans[0][0])
         else:
-            return False, ans[0][0]
+            response = ans[0][0] > self.THRESHOLD
+
+        return response
