83285a6042577b8f15b8e807fd5d79332d3a477f
rishi-e-kumar <47284667+rishi-e-kumar@users.noreply.github.com>
2022-02-10 18:17:05 -0800

    working individual

--- frgpascal/hardware/hotplate.py
+++ frgpascal/hardware/hotplate.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import hot
 import serial
 import numpy as np
 import os
@@ -8,6 +9,7 @@ from threading import Lock
 from frgpascal.hardware.geometry import Workspace
 from frgpascal.hardware.gantry import Gantry
 from frgpascal.hardware.gripper import Gripper
+from frgpascal.hardware.helpers import get_port
 
 MODULE_DIR = os.path.dirname(__file__)
 HOTPLATE_VERSIONS_DIR = os.path.join(MODULE_DIR, "versions", "hotplates")
@@ -132,6 +134,141 @@ class OmegaHub:
         return payload
 
 
+class Omega:
+    def __init__(self, id: int, port: str = None):
+        if id not in [1, 2, 3]:
+            raise ValueError("Hotplate ID must be 1 (green), 2 (pink), or 3 (blue)!")
+        self.address = id
+        constants = hotplateconstants[f"hp{id}"]
+        if port is None:
+            self.port = get_port(constants["device_identifiers"])
+        self.connect()
+        self.lock = Lock()  # for multithreaded access control
+
+    @property
+    def setpoint(self):
+        self.__setpoint = self.get_setpoint()
+        return self.__setpoint
+
+    @setpoint.setter
+    def setpoint(self, x):
+        if self.set_setpoint(setpoint=x):
+            self.__setpoint = x
+        else:
+            self.__setpoint = self.get_setpoint()
+            print(
+                "Error changing set point - set point is still {0} C".format(
+                    self.__setpoint
+                )
+            )
+
+    @property
+    def temperature(self):
+        return self.get_temperature()
+
+    def query(self, payload):
+        with self.lock:
+            self.__handle.write(payload)
+            response = self.__handle.readline()
+        return response
+
+    def connect(self):
+        self.__handle = serial.Serial()
+        self.__handle.port = self.port
+        self.__handle.timeout = 2
+        self.__handle.parity = "E"
+        self.__handle.bytesize = 7
+        self.__handle.baudrate = 9600
+        self.__handle.open()
+
+        # configure communication bits
+        self.__end = b"\r\n"  # end bit <etx>
+
+        # read current setpoint
+        # self.__setpoint = self.__setpoint_get()
+        # self.__setpoint = None
+
+        return True
+
+    def disconnect(self):
+        self.__handle.close()
+        return True
+
+    def get_temperature(self):
+        numWords = 1
+
+        payload = self.__build_payload(
+            address=self.address, command=3, dataAddress=1000, content=numWords
+        )
+        response = self.query(payload)
+
+        data = int(response[7:-4], 16) * 0.1  # response given in 0.1 C
+
+        return round(
+            data, 2
+        )  # only give two decimals, rounding error gives ~8 decimal places of 0's sometimes
+
+    def get_setpoint(self):
+        numWords = 1
+
+        payload = self.__build_payload(
+            address=self.address, command=3, dataAddress=1001, content=numWords
+        )
+        response = self.query(payload)
+
+        data = int(response[7:-4], 16) * 0.1  # response given in 0.1 C
+
+        return data
+
+    def set_setpoint(self, setpoint):
+        setpoint = round(setpoint * 10)  # need to give integer values of 0.1 C
+
+        payload = self.__build_payload(
+            address=self.address, command=6, dataAddress=1001, content=setpoint
+        )
+        response = self.query(payload)
+
+        if response == payload:
+            return True
+        else:
+            return False
+
+    ### helper methods
+    def __numtohex(self, num):
+        # return codecs.encode(str.encode('{0:02d}'.format(num)), 'hex_codec')
+        return "{0:02X}".format(num).encode()
+
+    def __build_payload(self, address, command, dataAddress, content):
+        def calculateChecksum(payload):
+            numHexValues = int(len(payload) / 2)
+            hexValues = [
+                int(payload[2 * i : (2 * i) + 2], 16) for i in range(numHexValues)
+            ]
+            checksum_int = (
+                256 - sum(hexValues) % 256
+            )  # drop the 0x convention at front, we only want the last two characters
+            checksum = "{0:02X}".format(checksum_int)
+
+            return str.upper(checksum).encode()
+
+        payload = self.__numtohex(address)
+        payload = payload + self.__numtohex(command)
+        payload = payload + str.encode(str(dataAddress))
+        payload = payload + "{0:04X}".format(content).encode()
+
+        # calculate checksum from current payload
+        chksum = calculateChecksum(payload)
+
+        # complete the payload
+        payload = payload + chksum
+        payload = payload + self.__end
+        payload = (
+            b":" + payload
+        )  # should start with ":", just held til the end to not interfere with checksum calculation
+
+        return payload
+
+
 class HotPlate(Workspace):
     def __init__(
         self,
@@ -145,11 +282,7 @@ class HotPlate(Workspace):
     ):
         constants, workspace_kwargs = self._load_version(version)
         super().__init__(
-            name=name,
-            gantry=gantry,
-            gripper=gripper,
-            p0=p0,
-            **workspace_kwargs,
+            name=name, gantry=gantry, gripper=gripper, p0=p0, **workspace_kwargs,
         )
         xmean = np.mean([p[0] for p in self._coordinates.values()])
         ymean = np.mean([p[1] for p in self._coordinates.values()])
